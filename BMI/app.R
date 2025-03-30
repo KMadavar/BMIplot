b3b1799baa7bce028380dccbf290febbf877dd88
Karthik Madavarapu <karthik.medavarapu@gmail.com>
2025-03-30 07:31:33 -0400

    initial commit

---
+++ BMI/app.R
@@ -0,0 +1,91 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+library(tidyverse)
+library(DT)
+library(bslib)
+#Define UI
+
+ui <- fluidPage( 
+  titlePanel("BMI Calculator"), 
+  sidebarLayout( 
+    sidebarPanel( 
+      #selectInput("weight", "Select Weight (kg):", choices = seq(40, 150, by = 1)), 
+      #selectInput("height", "Select Height (cm):", choices = seq(140, 210, by = 1)) ), 
+      numericInput( 
+        "weight", 
+        "Enter Weight", 
+        value = 1, 
+        min = 40, 
+        max = 150 
+      ), 
+      numericInput( 
+        "height", 
+        "Enter Height", 
+        value = 1, 
+        min = 140, 
+        max = 210 
+      ),
+      actionButton('submit', 'Submit the data')
+    ),
+    card( tags$h1('Bar Chart of BMI'),
+          tags$br(),
+          plotOutput("bmiPlot"), 
+          tags$br(),
+          tags$br(),
+          tags$h2('BMI Calculated Value'),
+          tags$br(),
+          verbatimTextOutput("bmiValue"), 
+          tags$br(),
+          tags$br(),
+          tags$h3('Table of height, weight, BMI'),
+          tags$br(),
+          dataTableOutput("table") 
+    )
+  )
+  
+)
+
+#Define server logic
+
+server <- function(input, output) { 
+  bmi <- reactive({ weight <- input$weight
+  height <- input$height / 100
+  # Convert cm to meters
+  bmi_value <- weight / (height^2)
+  return(bmi_value) })
+  
+  output$bmiValue <- renderText({
+    bmi_val <- bmi()
+    category <- ifelse(bmi_val < 18.5, "Underweight",
+                       ifelse(bmi_val < 24.9, "Normal weight",
+                              ifelse(bmi_val < 29.9, "Overweight", "Obese")))
+    paste("Your BMI is:", round(bmi_val, 2), "-", category)
+  }) %>% bindEvent(input$submit)
+  
+  bmidf <- reactive({
+    data.frame(category=c('Height','Weight','BMI'),
+               values=c(input$height , input$weight, bmi()))
+  })
+  
+  output$bmiPlot <- renderPlot({
+    ggplot2::ggplot(bmidf(), aes(x=category, y=values))+
+      geom_col()
+  }) %>% bindEvent(input$submit)
+  
+  output$table <- 
+    renderDataTable({datatable(bmidf())
+    }) %>% bindEvent(input$submit)
+}
+
+#Run the application
+
+shinyApp(ui = ui, server = server)
+
